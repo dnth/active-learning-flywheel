585c405df6719ed7f6dbd6ad2ce40a1089ede43d
dnth <dickson.neoh@gmail.com>
2025-01-24 22:16:27 +0800

    remove script

--- docs/scripts/gen_homepage.py
+++
@@ -1,7 +0,0 @@
-import mkdocs_gen_files
-
-with mkdocs_gen_files.open("../../README.md", "r") as f:
-    readme_content = f.read()
-
-with mkdocs_gen_files.open("../index.md", "w") as f:
-    f.write(readme_content)
